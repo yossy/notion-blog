f447c7ae54549f771087856413a6a8bb54a8bada
yossy0806 <t.yoshimura0806@gmail.com>
2020-11-08 21:13:09 +0900

    見出しの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link'
 import Header from '../components/header'
-import ExtLink from '../components/ext-link'
 import Features from '../components/features'
-import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
 export default () => (
@@ -16,16 +13,6 @@ export default () => (
         alt="Vercel + Notion"
       />
       <h1>My Notion Blog</h1>
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
 
       <Features />
 
